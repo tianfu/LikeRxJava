863ecc6dad7eeece283e90cfd8e4040a105c1398
mesmerize <5060265@qq.com>
2018-04-17 16:39:08 +0800

    likeRxJava

---
+++ app/src/main/java/com/mesmerize/likerxjava/MainActivity.java
@@ -0,0 +1,18 @@
+package com.mesmerize.likerxjava;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
